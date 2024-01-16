fe1e0fcde493fbee5e34a1b782af9706cb9a4253
João Victor Miranda <78143661+SrJohn1@users.noreply.github.com>
2024-01-15 22:13:45 -0300

    add sound effect onclick

--- script.js
+++ script.js
@@ -8,4 +8,11 @@ function toggleMode() {
     // }
 
     html.classList.toggle('light');
+
+    // Cria um objeto de áudio
+    var audio = new Audio('assets/click-buton.wav'); // Substitua pelo caminho do seu arquivo de som
+    audio.volume = 0.5;
+    // Reproduz o som
+    audio.play();
+
 }
