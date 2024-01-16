4fa9306846f1b64ebd609222f05b76e38fb082e8
João Victor Miranda <78143661+SrJohn1@users.noreply.github.com>
2024-01-15 23:42:17 -0300

    add adjustment server

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Indica que os arquivos estáticos serão servidos a partir do diretório principal
-app.use(express.static(__dirname));
+// Indica que os arquivos estáticos serão servidos a partir da pasta 'pages'
+app.use(express.static(path.join(__dirname, 'pages')));
 
 // Rota para servir o arquivo index.html quando acessar a raiz
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Rota para servir o arquivo consulta.html quando acessar /consulta
+app.get('/consulta', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'consulta', 'consulta.html'));
 });
 
 // Inicia o servidor na porta especificada
